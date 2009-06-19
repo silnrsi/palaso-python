9aeab9dbc81287a1b19905afa82312cd1bd75dac
Martin Hosken <mhosken@users.noreply.github.com>
2009-06-19 12:03:43 +0700

    Add icu and sortldml

---
+++ lib/palaso/collation/tailor.py
@@ -0,0 +1,194 @@
+"""Module to handle sort tailorings"""
+
+import xml.sax, sys, palaso.reggen
+from xml.sax.xmlreader import AttributesImpl
+from xml.sax.saxutils import XMLGenerator
+
+class Duplicate(Exception) : pass
+class Multiple(Exception) : pass
+
+class LDMLHandler(xml.sax.ContentHandler) :
+    def __init__(self, *args) :
+        xml.sax.ContentHandler.__init__(self, *args)
+        self.collations = []
+        self.text = ""
+        self.textelement = ""
+        self.reorders = []
+    def startElementNS(self, nsname, qname, attributes) :
+        ns = 'www.palaso.org/ldml/0.1'
+        tag = nsname[1]
+        if nsname[0] == ns and nsname[1] == 'reorder' :
+            self.currcollation.reorders.append((attributes.getValueByQName(attributes.getQNameByName((ns, 'match'))), attributes.getValueByQName(attributes.getQNameByName((ns, 'reorder')))))
+        elif tag == 'collation' :
+            self.currcollation = Collation(attributes.get('alt'))
+            self.collations.append(self.currcollation)
+        elif tag == 'settings' :
+            for q in attributes.getNames() :
+                v = attributes.getValue(q)
+                self.currcollation.settings[q[1]] = v
+        elif tag in ('first_variable', 'last_variable', 'first_tertiary_ignorable', 'last_tertiary_ignorable', 'first_secondary_ignorable', 'last_secondary_ignorable', 'first_primary_ignorable', 'last_primary_ignorable', 'first_non_ignorable', 'last_non_ignorable', 'first_trailing', 'last_trailing') :
+            self.textelement = tag
+        elif tag == 'cp' :
+            self.text += unichr(attributes.get('hex'))
+        elif tag == 'reset' :
+            self.resetattr = attributes.get('before')
+    def endElementNS(self, nsname, qname) :
+        tag = nsname[1]
+        self.text = self.text.strip()
+        if tag == 'reset' :
+            if self.textelement :
+                self.currelement = self.currcollation.addreset(self.textelement, 1)
+            else :
+                self.currelement = self.currcollation.addreset(self.text, 0)
+            if self.resetattr :
+                self.currelement.addbefore(self.resetattr[0])
+                self.resetattr = None
+        elif tag in ('p', 's', 't', 'q', 'i') :
+            self.currelement = self.currelement.addElement(self.text, tag)
+        elif tag in ('pc', 'sc', 'tc', 'qc', 'ic') :
+            for c in self.text :
+                self.currelement = self.currelement.addElement(c, tag[0])
+        self.text = ''
+    def characters(self, data) :
+        self.text += data
+    def asLDML(self, sax) :
+        sax.startElement('collations', AttributesImpl({}))
+        for c in self.collations :
+            c.asLDML(sax)
+        sax.endElement('collations')
+
+class Collation :
+    icu_relation_values = {'r' : 0, 'p' : 1, 's' : 2, 't' : 3, 'q' : 4, 'i' : 5}
+    def __init__(self, type) :
+        self.settings = {}
+        self.rules = []
+        self.type = type
+        self.reorders = []
+    def addreset(self, value, isSpecial) :
+        element = Element(self)
+        if isSpecial :
+            element.special = value
+        else :
+            element.string = value
+        element.relation = 'r'
+        self.rules.append(element)
+        return element
+    def asICU(self) :
+        res = ""
+        for k, v in self.settings.items() :
+            res = res + "[" + k + " " + v + "]"
+        for r in self.rules :
+            res += r.asICU()
+            res += "\n"
+        return res
+    def asLDML(self, sax) :
+        sax.startElement('collation', AttributesImpl({'type' : self.type} if self.type else {}))
+        if len(self.settings) > 0 :
+            sax.startElement('settings', AttributesImpl(self.settings))
+            sax.endElement('settings')
+        sax.startElement('rules', AttributesImpl({}))
+        for r in self.rules :
+            r.asLDML(sax)
+        sax.endElement('rules')
+        sax.endElement('collation')
+    def getElements(self) :
+        allstr = set()
+        for r in self.rules :
+            for e in r :
+                for i in range(0, len(e.string)) :
+                    allstr.add(e.string[0:i+1])
+        return allstr
+    def flattenOrders(self, debug = 0) :
+        types = 'i', 'p', 's', 't', 'q'
+        tailor = self.asICU()
+        results = []
+        for r in self.reorders :
+            for b, s in palaso.reggen.expand_sub(r[0], r[1], debug=debug) :
+                e = self.addreset(s, 0)
+                e.addElement(b, 'i')
+                if debug :
+                    print ("%s -> %s" % (b, s)).encode("utf-8")
+    def testPrimaryMultiple(self) :
+        ces = set()
+        res = []
+        for r in self.rules :
+            min = 10
+            ok = (len(r.string) <= 1 or r.string in ces)
+            for e in r :
+                if e.relation != 'r' and e.string != '':
+                    ces.add(e.string)
+                val = self.icu_relation_values.get(e.relation, 0)
+                if val > 0 and val < min : min = val
+            if not ok and min == 1 :
+                res.append(r.string)
+        return res
+
+class Element :
+    icu_relation_map = {
+        'r' : '&',
+        'p' : '<',
+        's' : '<<',
+        't' : '<<<',
+        'q' : '=',
+        'i' : '=' }
+    def __init__(self, parent) :
+        self.parent = parent
+        self.special = None
+        self.string = ""
+        self.child = None
+    def __iter__(self) : return ElementIter(self)
+    def addElement(self, string, type) :
+        element = Element(self)
+        element.string = string
+        element.relation = type
+        self.child = element
+        return element
+    def asICU(self) :
+        res = Element.icu_relation_map[self.relation]
+        if self.special :
+            res += "[" + self.special.replace('_', ' ') + "]"
+        else :
+            res += self.string
+        if self.child :
+            res += self.child.asICU()
+        return res
+    def asLDML(self, sax) :
+        tag = 'reset' if self.relation == 'r' else self.relation
+        sax.startElement(tag, AttributesImpl({}))
+        if self.special :
+            sax.startElement(self.special, AttributesImpl({}))
+            sax.endElement(self.special)
+        else :
+            sax.characters(self.string)
+        sax.endElement(tag)
+        if self.child :
+            self.child.asLDML(sax)
+
+class ElementIter :
+    def __init__(self, start) :
+        self.curr = start
+    def __iter__(self) : return self
+    def next(self) :
+        res = self.curr
+        if not res : raise StopIteration()
+        self.curr = res.child
+        return res
+
+if __name__ == "__main__" :
+    filename = sys.argv[1]
+    print filename
+    handler = LDMLHandler()
+    parser = xml.sax.make_parser()
+    parser.setContentHandler(handler)
+    parser.setFeature(xml.sax.handler.feature_namespaces, 1)
+    parser.setFeature(xml.sax.handler.feature_namespace_prefixes, 1)
+    parser.parse(filename)
+    outsax = XMLGenerator()
+    for c in handler.collations :
+        print "---------- %s ----------" % (c.type)
+        if len(c.reorders) > 0 :
+            c.flattenOrders()
+        print c.asICU().encode('utf-8')
+
+    handler.asLDML(outsax)
+
---
+++ lib/palaso/unicsv.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+# Author:  Tim Eves (tim_eves@sil.org)
+# Version: 20090525
+
+import codecs, csv
+
+from csv import QUOTE_MINIMAL, QUOTE_ALL, QUOTE_NONNUMERIC, QUOTE_NONE, \
+                Error, Dialect, excel, excel_tab, field_size_limit, \
+                register_dialect, get_dialect, list_dialects, \
+                unregister_dialect, __version__, __doc__
+
+
+__all__ = [ "QUOTE_MINIMAL", "QUOTE_ALL", "QUOTE_NONNUMERIC", "QUOTE_NONE",
+            "Error", "Dialect", "Sniffer", "excel", "excel_tab", "reader", "writer",
+            "register_dialect", "get_dialect", "list_dialects",
+            "unregister_dialect", "__version__", "DictReader", "DictWriter" ]
+
+
+def _utf8_recoder(f,enc):
+    return f if enc in ('utf_8','U8','UTF','utf8') 
+            else codecs.EncodedFile(f,'utf_8',enc) 
+
+class reader:
+    def __init__(self, f, dialect=excel, encoding='utf-8', *args, **kwds):
+        self.__reader = csv.reader(_utf8_recoder(f,encoding), dialect, *args, **kwds)
+    
+    def next(self):
+        return tuple(unicode(cell,'utf-8') for cell in self.__reader.next())
+    
+    def __iter__(self):
+        return self
+    
+    @property
+    def dialect(self): return self.__reader.dialect
+    
+    @property
+    def line_num(self): return self.__reader.line_num
+
+
+class writer:
+    def __init__(self, f, dialect=excel, encoding='utf-8', *args, **kwds):
+        self.__writer = csv.writer(_utf8_recoder(f,encoding), dialect, *args, **kwds)
+    
+    def writerow(self,row):
+        self.__writer.writerow(cell.encode('utf-8') for cell in row)
+    
+    def writerows(self,rows):
+        self.__writer.writerows([cell.encode('utf-8') for cell in row] for row in rows)
+    
+    @property
+    def dialect(self): return self.__writer.dialect
+
+
+
+class DictReader:
+    def __init__(self, f, fieldnames=None, restkey=None, restval=None, dialect=excel, encoding='utf-8', *args, **kwds):
+        self.__reader=csv.DictReader(_utf8_recoder(f,encoding), dialect=dialect, *args, **kwds)
+    
+    def __iter__(self): return self
+    
+    def next(self):
+        return dict((unicode(k,'utf-8'),unicode(v,'utf-8')) for k,v in self.__reader.next().iteritems())
+    
+    @property
+    def dialect(self): return self.__reader.dialect
+    
+    @property
+    def line_num(self): return self.__reader.line_num
+    
+    @property
+    def fieldnames(self): return self.__reader.fieldnames
+
+
+class DictWriter:
+    def __init__(self, f, fieldnames, restval='', extrasaction='raise', dialect=excel, encoding='utf-8', *args, **kwds):
+        self.__writer=csv.DictWriter(_utf8_recoder(f,encoding), fieldnames, dialect=dialect, *args, **kwds)
+    
+    @staticmethod
+    def __make_row(row):
+        return dict((k.encode('utf-8'),v.encode('utf-8')) for k,v in row.iteritems())
+    
+    def writerow(self,row):
+        self.__writer.writerow(self.__make_row(row))
+    
+    def writerows(self,rows):
+        self.__writer.writerows(self.__make_row(row) for row in rows)
+    
+    @property
+    def dialect(self): return self.__writer.dialect
+
+
+class Sniffer(csv.Sniffer):
+    def sniff(self, sample, delimiters=None, encoding='utf-8'):
+        """
+        Returns a dialect (or None) corresponding to the sample
+        """
+        csv.Sniffer.sniff(self, sample.decode(encoding).encode('utf-8') if encoding != 'utf-8' else sample)
+    
+    def has_header(self, sample, encoding='utf-8'):
+        csv.Sniffer.has_header(self, sample.decode(encoding).encode('utf-8') if encoding != 'utf-8' else sample)
+
