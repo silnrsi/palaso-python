a8600150b07f09fe45902a8fff57d6564abbe861
Tim Eves <tim-eves@users.noreply.github.com>
2009-06-22 12:54:44 +0700

    Refactor Mapping class to sublcass from builtin str

--- lib/palaso/teckit/engine.py
+++ lib/palaso/teckit/engine.py
@@ -11,42 +11,65 @@
 from __future__ import with_statement
 
 import _engine
+import codecs
 from _engine import getVersion
 from _engine import Option
 from _engine import \
     CompilationError, ConverterBusy, MappingVersionError, \
-    FullBuffer, EmptyBuffer, flags
+    FullBuffer, EmptyBuffer, flags, Form
 
 
-class Mapping(object):
-    def __init__(self, path):
+class Mapping(str):
+    def __new__(cls, path):
         with open(path, 'rb') as mf:
-            self.__table = mf.read()
+            data = mf.read()
+        return str.__new__(cls,data)
     
+    def __init__(self, *args):
+        self.__args = args
+        
     def __getattr__(self, name):
         from _engine import getMappingName
         try:
             nid = getattr(_engine.NameID,name)
-            nlen = getMappingName(self.__table, len(self.__table), nid)
+            nlen = getMappingName(self, len(self), nid)
         except (AttributeError,IndexError):
             raise AttributeError('%r object has no attribute %r' % (self,name))
         buf  = _engine.create_string_buffer(nlen)
-        nlen = getMappingName(self.__table, len(self.__table), nid, buf, nlen)
+        nlen = getMappingName(self, len(self), nid, buf, nlen)
         return str(buf[:nlen])
+    
+#    def __len__(self):  return len(self.__table)
+#    
+    def __str__(self):  return self.lhsName + ' <-> ' + self.rhsName
+    
+    def __repr__(self): return 'Mapping(%r)' % self.__args
     
     @property
     def lhsFlags(self):
-        return _engine.getMappingFlags(self.__table, len(self.__table))[0]
+        return _engine.getMappingFlags(self, len(self))[0]
     
     @property
     def rhsFlags(self):
-        return _engine.getMappingFlags(self.__table, len(self.__table))[1]
+        return _engine.getMappingFlags(self, len(self))[1]
+
+
+
+def _form_from_flags(form, flags):
+    if form==Form.Unspecified:
+        if   flags.expectsNFD or flags.generatesNFD:  form = Form.NFD
+        elif flags.expectsNFC or flags.generatesNFC:  form = Form.NFC
+    return form + Form.UTF8 if flags.unicode else Form.Bytes
 
 
 class Converter(object):
-    def __init__(self, mapping, forward=True, source=None, target=None):
-        self.__converter = _engine.createCoverter(mapping, len(mapping), forward, source, target)
+    def __init__(self, mapping, forward=True,source=Form.Unspecified,target=Form.Unspecified):
+        source = _form_from_flags(source, mapping.lhsFlags if forward else mapping.rhsFlags)
+        target = _form_from_flags(target, mapping.rhsFlags if forward else mapping.lhsFlags)
+        self.__converter = _engine.createConverter(mapping, len(mapping), forward, source, target)
         self.__flags = None
+        self.__residue = ''
+
     
     def __del__(self):
         self.close()
@@ -72,44 +95,65 @@ class Converter(object):
     def sourceFlags(self):
         if not self.__flags:
             self.__flags = _engine.getConverterFlags(self.__converter)
-        return Form(self.__flags[0])
+        return self.__flags[0]
     
     @property
     def targetFlags(self):
         if not self.__flags:
             self.__flags = _engine.getMappingFlags(self.__converter)
-        return Form(self.__flags[1])
+        return self.__flags[1]
     
     
     def reset(self):
         _engine.resetConverter(self.__converter)
+        self.__residue = ''
     
     
-    def convert(self, data, options=Option.UseReplacementCharSilently):
-        buf = _engine.create_string_buffer(long(len(data)*2/3))
-        cons = 0
-        while cons < len(data):
+    def convert(self, data, finished=False, options=Option.UseReplacementCharSilently):
+        if self.sourceFlags.unicode and isinstance(data,str):
+            raise TypeError("data is type 'str' but unicode is expected")
+        
+        options += Option.InputIsComplete if finished else 0
+        if self.sourceFlags.unicode:
+            data = codecs.encode(data,'utf-8')
+        data = self.__residue + data
+        
+        buf = _engine.create_string_buffer(long(len(data)*3/2))
+        out = 0
+        while True:
             try:
+                print 'trying with buffer size %d' % len(buf)
                 cons,outs,lhc = _engine.convertBufferOpt(self.__converter, data, len(data), buf, len(buf), options)
-            except FullBuffer:
-                _engine.resize(buf, long(len(buf)*2/3))
+                if finished:
+                    self.reset()
+                break
+            except FullBuffer, (cons,outs,lhc):
+                print (cons,outs,lhc)
+                print repr(unicode(str(buf[:outs]),'utf-8') + self.flush(finished,options))
+                buf=_engine.create_string_buffer(long(len(buf)*3/2))
+            except EmptyBuffer, (cons,outs,lhc):
+                break
         
-        if options & InputIsComplete:
-            self.reset()
-        return str(buf[:outs])
+        self.__residue = data[cons:]
+        
+        buf = str(buf[:outs])
+        return unicode(buf,'utf-8') if self.targetFlags.unicode else buf
     
     
-    def flush(self,options=Option.UseReplacementCharSilently):
-        buf = _engine.create_string_buffer(size=long(len(data)*2/3))
-        cons = 0
-        while cons < len(data):
+    def flush(self,finished=True,options=Option.UseReplacementCharSilently):
+        options += Option.InputIsComplete if finished else 0
+        buf = _engine.create_string_buffer(128L)
+        outs = 0
+        while True:
             try:
-                outs,lhc = _engine.flush(self.__converter, buf, len(buf), options | Option.InputIsComplete)
+                outs,lhc = _engine.flushOpt(self.__converter, buf, len(buf), options)
+                if finished:
+                    self.reset()
+                break
             except FullBuffer:
-                buf = _engine.create_string_buffer(size=long(len(buf)*2/3))
-        
-        if complete:
-            self.reset()
-        
-        return str(buf[:outs])
+                print (cons,outs,lhc)
+                print repr(unicode(str(buf[:outs]),'utf-8'))
+                buf=_engine.create_string_buffer(long(len(buf)*3/2))
 
+        buf = str(buf[:outs])
+        return unicode(buf,'utf-8') if self.targetFlags.unicode else buf
