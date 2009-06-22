229acb335262789720cd2a0359ca6bdbe210169f
tim <tim@localhost>
2009-06-22 12:54:44 +0700

    Refactor Mapping class to sublcass from builtin str

--- lib/palaso/teckit/compiler.py
+++ lib/palaso/teckit/compiler.py
@@ -21,15 +21,11 @@ def _err_fn(user_data, msg, param, line):
 
 
 class _Compiled(Mapping):
-    def __init__(self, txt, opts=Opt.Compress):
-        if txt:
-            (tbl, tbl_len) = _compiler.compileOpt(txt, len(txt),
-                teckit_error_fn(_err_fn, ), None, opts)
-            self.__table = tbl
-        else:
-            self.__table = None
+    def __new__(cls,  txt, opts=Opt.Compress):
+        (tbl, tbl_len) = _compiler.compileOpt(txt, len(txt), teckit_error_fn(_err_fn, ), None, opts)
+        return super(Mapping,cls).__new__(tbl)
     
-    
+        
     def __del__(self):
         self.close()
     
