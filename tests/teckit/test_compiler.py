b5242384c91a90b56e0c13d64a14a152df1fcb73
Tim Eves <tim-eves@users.noreply.github.com>
2009-06-25 20:26:53 +0700

    Add tests for compile to XML bindings

--- tests/teckit/test_compiler.py
+++ tests/teckit/test_compiler.py
@@ -2,20 +2,30 @@ from __future__ import with_statement
 
 import unittest
 import os.path
-from palaso.teckit.compiler import compile
+from palaso.teckit.compiler import compile, translate
 from palaso.teckit.engine   import Converter, Form, Mapping
 
 class TestCompiler:
     def test_compilation(self):
+        if not hasattr(self, 'reference_file'): return
         ref_map = Mapping(os.path.join('data',self.reference_file))
-        source = open(os.path.join('data',self.source_file),'rb').read()
+        source  = open(os.path.join('data',self.source_file),'rb').read()
         com_map = compile(source, self.compress)
         self.assertEqual(str(com_map), str(ref_map))
         self.assertEqual(com_map, ref_map,
                          'compiled %r (%scompressed) does not match reference %r' % 
                             (self.source_file, '' if self.compress else 'un', self.reference_file))
         del com_map
-        del ref_map
+
+    def test_translation(self):
+        if not hasattr(self, 'reference_file_xml'): return
+        ref_map = open(os.path.join('data',self.reference_file_xml)).read()
+        source  = open(os.path.join('data',self.source_file),'rb').read()
+        com_map = translate(source)
+        self.assertEqual(com_map, ref_map,
+                         'translated %r (xml) does not match reference %r' % 
+                            (self.source_file, self.reference_file_xml))
+        del com_map
 
 
 class CompileGreekMapping(TestCompiler):
@@ -29,6 +39,10 @@ class CompileGreekMappingUncrompressed(unittest.TestCase, CompileGreekMapping):
 class CompileGreekMappingCompressed(unittest.TestCase, CompileGreekMapping):
     reference_file = 'SILGreek2004-04-27.reference.tec'
     compress       = True
+
+class TranslateISO_8859_1_XML(unittest.TestCase, TestCompiler):
+    source_file    = 'ISO-8859-1.map'
+    reference_file_xml = 'ISO-8859-1.map.reference.xml'
 
 
 if __name__ == "__main__":
