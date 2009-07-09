fc4261e7f5bb2f1f3be87c3a6d6d2d9235c2864f
Martin Hosken <martin_hosken@sil.org>
2009-07-09 10:52:24 +0700

    Add relaxedsubs context manager and preprocessing to collation testing

--- lib/palaso/collation/icu.py
+++ lib/palaso/collation/icu.py
@@ -1,15 +1,21 @@
 """Module to interact with PyICU"""
 
-import PyICU, operator, struct
+import PyICU, operator, struct, re, palaso.reggen, palaso.contexts
 
-def sorted(tailor, strs, level=15) :
+def sorted(tailor, strs, level=15, preproc=()) :
     """sorts a list of strings against the given tailoring and level, returning the resultant sorted list.
 Level = 0 - PRIMARY, 1 - SECONDARY, 2 - TERTIARY, 3 - QUARTENARY, 15 - IDENTICAL. Defaults to IDENTICAL
 """
+    procreg = [(re.compile(p[0]), p[1]) for p in preproc]
     collator = PyICU.RuleBasedCollator(tailor, level)
-    results = [(collator.getCollationKey(s), s) for s in strs]
+    results = [(_sortkey(collator, procreg, s), s) for s in strs]
     results.sort(cmp = lambda a, b: a.compareTo(b), key=operator.itemgetter(0))
     return [s[1] for s in results]
+
+def _sortkey(coll, proc, str) :
+    with palaso.contexts.relaxedsubs() :
+        str = reduce(lambda s, p : p[0].sub(p[1], s), proc, str)
+    return coll.getCollationKey(str)
 
 def strkey(key) :
     """Returns a string representation of a collation key"""
