29ac3c54c2ae2cb19d2d2e6f3a0078e11df9848b
Tim Eves <tim-eves@users.noreply.github.com>
2009-09-22 14:40:55 +0700

    'Release' palaso.kmn Bump the version number and add palaso.kmn to the list of packages in setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ from Pyrex.Distutils import build_ext
 
 
 setup(name='palaso',
-      version='0.2.0b1',
+      version='0.3.0b1',
       description='Payap Language Software python package and scripts',
       long_description="Modules and scripts useful for building language software.",
       maintainer='Tim Eves',
       maintainer_email='tim_eves@sil.org',
       url='http://projects.palaso.org/projects/show/palaso-python',
-      packages=['', 'palaso', 'palaso.collation'],
+      packages=['', 'palaso', 'palaso.collation', 'palaso.kmn'],
       ext_modules = [
         Extension("palaso.kmfl", ["lib/palaso.kmfl.pyx"], libraries=["kmfl", "kmflcomp"])
         ],
