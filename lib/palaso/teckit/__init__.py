547cbf3d4a4e048a9299aedd753d480fa4b42032
Tim Eves <tim-eves@users.noreply.github.com>
2010-04-23 18:41:34 +0700

    Complete the python endoing framework integration Added support for codecs error handlers and added many test cases to demonstrate it. Extended the test case to match the same set of operations as the teckit.engine test cases cover, including incremental and encode as well as decode.

--- lib/palaso/teckit/__init__.py
+++ lib/palaso/teckit/__init__.py
@@ -8,73 +8,132 @@ __all__ = ['engine','compiler']
 
 _tec_mapping_cache_ = {}
 
+class _mapping(object):
+    def __init__(self, mapping):
+        self.map = mapping
+        self.replacement = None
+
 def register(mapping):
     if isinstance(mapping, (str,unicode)):
         mapping=engine.Mapping(mapping)
     elif not isinstance(mapping,engine.Mapping):
         raise TypeError('object %r is not type engine.Mapping' % mapping)
     
-    _tec_mapping_cache_[('%s<->%s' % (mapping.lhsName,mapping.rhsName)).lower()] = mapping
+    _tec_mapping_cache_[('%s<->%s' % (mapping.lhsName,mapping.rhsName)).lower()] = _mapping(mapping)
 
 def register_library(path):
     map(register, glob.glob(os.path.join(path,'*.tec')))
 
 def list_mappings():
-    return _tec_mapping_cache_
+    return dict((n,repr(m.map)) for (n,m) in _tec_mapping_cache_.iteritems())
 
 def _teckit_search(mapping):
     mapobj = _tec_mapping_cache_.get(mapping)
-    if mapobj :
-        _dec = engine.Converter(mapobj)
-        _enc = engine.Converter(mapobj, forward = False)
-        return codecs.CodecInfo(
-            name = mapobj.__str__(),
-            encode = _enc.encode,
-            decode = _dec.decode,
-            incrementalencoder = _CreateIncrementalEncoder(_enc),
-            incrementaldecoder = _CreateIncrementalDecoder(_dec),
-            streamreader = _CreateStreamReader(_dec),
-            streamwriter = _CreateStreamWriter(_enc))
-    else : return None 
+    if not mapobj: return None
 
-_errors = {
-    'strict':Option.DontUseReplacementChar,
-    'ignore':Option.DontUseReplacementChar,
-    'replace':Option.UseReplacementCharSilently,
-    'xmlcharrefreplace':Option.UseReplacementCharWithWarning,
-    'xmlcharrefreplace':Option.UseReplacementCharWithWarning
-    }
+    def incrementalencoder(errors='strict'): return IncrementalEncoder(mapobj, errors)
+    def incrementaldecoder(errors='strict'): return IncrementalDecoder(mapobj, errors)
+    def streamreader(stream, errors='strict'):return StreamReader(mapobj, stream, errors)
+    def streamwriter(stream, errors='strict'):return StreamWriter(mapobj, stream, errors)
+
+    codec = Codec(mapobj)            
+    return codecs.CodecInfo(
+        name = mapobj.__str__(),
+        encode = codec.encode,
+        decode = codec.decode,
+        incrementalencoder = incrementalencoder,
+        incrementaldecoder = incrementaldecoder,
+        streamreader = streamreader,
+        streamwriter = streamwriter)
 
 
+class Codec(codecs.Codec):
+    def __init__(self, mapping):
+        self.__enc = engine.Converter(mapping.map, forward=False)
+        self.__dec = engine.Converter(mapping.map, forward=True)
+        mapping.replacement = self.__dec.convert(
+                                self.__enc.convert(u'\ufffd', finished=True),
+                                finished=True)
+        self.__enc.reset()
+        
+    
+    @staticmethod
+    def convert(conv, data, final, errors='strict'):
+        try:
+            res = conv.convert(data, finished=final, 
+                               options=Option.DontUseReplacementChar)
+            return (res,len(res))
+        except UnicodeEncodeError, uerr:
+            rep,rp = codecs.lookup_error(errors)(uerr)
+            try:
+                prefix = conv.convert(uerr.object[:uerr.start] + rep, finished=final, 
+                                      options=Option.DontUseReplacementChar)
+            except UnicodeEncodeError:
+                raise UnicodeEncodeError(*(uerr.args[:4] + ('cannot convert replacement %r to target encoding' % rep,)))
+            suffix = Codec.convert(conv, data[rp:], final, errors)
+            return (prefix+suffix[0],rp+suffix[1])
+        except UnicodeDecodeError, uerr:
+            rep,rp = codecs.lookup_error(errors)(uerr)
+            prefix = conv.convert(uerr.object[:uerr.start], finished=final, 
+                                  options=Option.DontUseReplacementChar)
+            suffix = Codec.convert(conv, data[rp:], final, errors)
+            return (prefix+rep+suffix[0],rp+suffix[1])
+        
 
-# Don't need to store residue in these objects since new converter
-# created for each instance, so store state in the converter
-def _CreateIncrementalEncoder(converter) :
-    class _IncrementalEncoder(codecs.IncrementalEncoder) :
-        _conv = converter
-        def encode(self, input, final = False) :
-            return self._conv.convert(input, finished = final, options = _errors[self.errors])
-    return _IncrementalEncoder
+    def encode(self, input, errors='strict'):
+        return Codec.convert(self.__enc, input, True, errors)
 
-def _CreateIncrementalDecoder(converter) :
-    class _IncrementalDecoder(codecs.IncrementalDecoder) :
-        _conv = converter
-        def decode(self, input, final = False) :
-            return self._conv.convert(input, finished = final, options = _errors[self.errors])
-    return _IncrementalDecoder
+    def decode(self, input, errors='strict'):
+        return Codec.convert(self.__dec, input, True, errors)
+        
 
-def _CreateStreamWriter(converter) :
-    class _StreamWriter(codecs.StreamWriter) :
-        _conv = converter
-        def encode(self, stream, errors = 'strict') :
-            return self._conv.encode(stream, errors = _errors[errors])
-    return _StreamWriter
+class IncrementalCommon(object):
+    def __init__(self, forward, mapping, errors):
+        base = codecs.IncrementalDecoder if forward else codecs.IncrementalEncoder
+        base.__init__(self, errors)
+        self._conv = engine.Converter(mapping.map, forward)
+    
+    def convert(self, object, final=False):
+        return Codec.convert(self._conv, object, final, self.errors)[0]
+    
+    def reset(self):    return self._conv.reset()
+    
+    def getstate(self): return self._conv
+    
+    def setstate(self, state): return self._conv
 
-def _CreateStreamReader(converter) :
-    class _StreamReader(codecs.StreamReader) :
-        _conv = converter
-        def decode(self, stream, errors = 'strict') :
-            return self._conv.decode(stream, errors = _errors[errors])
-    return _StreamReader
+
+class IncrementalEncoder(IncrementalCommon, codecs.IncrementalEncoder):
+    def __init__(self, mapping, errors='strict'):
+        super(IncrementalEncoder,self).__init__(False, mapping, errors)
+
+    encode = IncrementalCommon.convert
+
+
+class IncrementalDecoder(IncrementalCommon, codecs.IncrementalDecoder):
+    def __init__(self, mapping, errors='strict'):
+        super(IncrementalDecoder,self).__init__(True, mapping, errors)
+
+    decode = IncrementalCommon.convert
+
+
+class StreamWriter(Codec, codecs.StreamWriter):
+    def __init__(self, mapping, stream, errors='strict'):
+        Codec.__init__(self, mapping)
+        codecs.StreamWriter.__init__(self, stream, errors)
+    
+class StreamReader(Codec, codecs.StreamReader):
+    def __init__(self, mapping, stream, errors='strict'):
+        Codec.__init__(self, mapping)
+        codecs.StreamReader.__init__(self, stream, errors)
+
+def teckit_replace_errors(exception):
+    if isinstance(exception, UnicodeDecodeError):
+        raise TypeError("don't know how to handle UnicodeDecodeError in error callback")
+    rep = _tec_mapping_cache_[exception.encoding].replacement
+    return (rep, exception.end)
 
 codecs.register(_teckit_search)
+codecs.register_error('teckitreplace', teckit_replace_errors)
+
+
