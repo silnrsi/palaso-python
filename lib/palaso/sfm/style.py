30ed9a7f48231897bc3911e6dcb07b15b58aaad8
Tim Eves <Tim Eves@localhost>
2010-10-11 08:39:47 +0700

    The STY style-sheet file parser module. This defines the database schema for STY database files necessary to drive the palaso.sfm.records parser. This is required by the new palaso.sfm.usfm parser

---
+++ lib/palaso/sfm/style.py
@@ -0,0 +1,64 @@
+'''
+The STY stylesheet file parser module.  This defines the database schema for 
+STY files necessary to drive the SFM DB parser.
+'''
+__version__ = '20101011'
+__date__    = '11 October 2010'
+__author__  = 'Tim Eves <tim_eves@sil.org>'
+__history__ = '''
+	20100111 - tse - Initial version
+	20101026 - tse - rewrote to use new palaso.sfm.records module
+'''
+import re
+import palaso.sfm.records as records
+from palaso.sfm.records import sequence,flag
+from itertools import imap
+from functools import partial
+
+
+
+_fields = {'Marker'         : (str,             SyntaxError('missing Marker marker missing')),
+           'Endmarker'      : (str,             None),
+           'Name'           : (str,             SyntaxError('Marker {0} defintion missing: {1}')),
+           'Description'    : (str,             SyntaxError('Marker {0} defintion missing: {1}')),
+           'OccursUnder'    : (sequence(str),   [None]),
+           'Rank'           : (int,             None),
+           'TextProperties' : (sequence(str),   []),
+           'TextType'       : (str,             SyntaxError('Marker {0} defintion missing: {1}')),
+           'StyleType'      : (str,             None),
+           'FontSize'       : (int,             None),
+           'Regular'        : (flag,            False),
+           'Bold'           : (flag,            False),
+           'Italic'         : (flag,            False),
+           'Underline'      : (flag,            False),
+           'Superscript'    : (flag,            False),
+           'Justification'  : (str,             'Left'),
+           'SpaceBefore'    : (int,             0),
+           'SpaceAfter'     : (int,             0),
+           'FirstLineIndent': (float,           0),
+           'LeftMargin'     : (float,           0),
+           'RightMargin'    : (float,           0),
+           'Color'          : (int,             0)
+           }
+
+
+
+_comment = re.compile(r'\s*#.*$')
+
+
+
+def _munge_record(r):
+    tag = r.pop('Marker')
+    ous = r['OccursUnder']
+    if 'NEST' in ous:
+        ous.remove('NEST')
+        ous.append(tag)
+    return (tag, r)
+
+
+def parse(source):
+    source = imap(partial(_comment.sub,''), source)
+    recs = iter(records.parser(source, records.schema('Marker', _fields)))
+    next(recs,None)
+    return dict(imap(_munge_record, recs))
+
