e9ca5664961dc24a1aef1283cfd67995c2b25688
Bobby de Vos <bobby_devos@sil.org>
2019-03-15 17:08:32 -0600

    Unicode 12 normalization test

--- tests/sldr/ldml_exemplars_test.py
+++ tests/sldr/ldml_exemplars_test.py
@@ -219,6 +219,14 @@ class UCDTests(unittest.TestCase):
         text = u'\u0061\u0315\u0300\u05AE\u09FE\u0062'
         self.assertEqual(u'\u0061\u05AE\u0300\u09FE\u0315\u0062', self.ucd.normalize('NFD', text))
 
+    def ignore_nfc_tus12(self):
+        text = u'\u0061\u0315\u0300\u05AE\U0001E136\u0062'
+        self.assertEqual(u'\u00E0\u05AE\U0001E136\u0315\u0062', self.ucd.normalize('NFC', text))
+
+    def ignore_nfd_tus12(self):
+        text = u'\u0061\u0315\u0300\u05AE\U0001E136\u0062'
+        self.assertEqual(u'\u0061\u05AE\u0300\U0001E136\u0315\u0062', self.ucd.normalize('NFD', text))
+
 
 class ExemplarsTests(unittest.TestCase):
 
