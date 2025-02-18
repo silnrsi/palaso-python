35618f8bed0239fe8473905fc226028578c42319
Tim Eves <tim_eves@sil.org>
2025-02-18 14:44:55 +0700

    Only built kmfl extension if the headers are installed

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
+from pathlib import Path
 
-setup(
-    ext_modules=cythonize(
+if Path("/usr/include/kmfl").exists():
+    extensions = cythonize(
         [Extension(
             name="palaso.kmfl",
             sources=["src/palaso.kmfl.pyx"],
             libraries=["kmfl", "kmflcomp"]
         )]
     )
-)
+else: 
+    extensions = []
+
+setup(ext_modules=extensions)
